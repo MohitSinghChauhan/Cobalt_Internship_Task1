09841aa19b9a9793c7e042a76d7620ff79c8fb9e
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-04-30 16:57:55 +0530

    Envelope Creation APIs Setup Successful

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const SECRET_KEY = crypto.randomBytes(32).toString('hex');
-
 app.use(
   session({
     secret: SECRET_KEY,
@@ -47,17 +46,84 @@ const tokenValidation = async (req, res) => {
       const jwtTokenResult = await dsApiClient.requestJWTUserToken(
         process.env.INTEGRATION_KEY,
         process.env.USER_ID,
-        'signature',
+        ['signature', 'impersonation'],
         fs.readFileSync(path.join(__dirname, 'private.key')),
         3600
       );
       console.log('New access token generated:', jwtTokenResult.body);
       req.session.accessToken = jwtTokenResult.body.access_token;
+      // console.log('Access token:', req.session.accessToken);
       req.session.accessTokenExpiration =
         Date.now() + (jwtTokenResult.body.expires_in - 60) * 1000;
     }
   }
 };
+
+// This function returns an instance of the EnvelopesApi with the API client configured
+const getEnvelopesApi = (req) => {
+  // Create a new instance of the DocuSign API client
+  const dsApiClient = new docusign.ApiClient();
+
+  // Set the base path for the client using the environment variable
+  dsApiClient.setBasePath(process.env.BASE_PATH);
+
+  // Add the access token to the default headers using the request session
+  dsApiClient.addDefaultHeader(
+    'Authorization',
+    'Bearer ' + req.session.accessToken
+  );
+
+  // Return an instance of the EnvelopesApi with the configured API client
+  return new docusign.EnvelopesApi(dsApiClient);
+};
+
+// This function creates and returns an instance of an envelope object for the provided recipient details
+const makeEnvelope = (name, email) => {
+  // Create a new instance of the EnvelopeDefinition object
+  const envelope = new docusign.EnvelopeDefinition();
+
+  // Set the template ID for the envelope using the environment variable
+  envelope.templateId = process.env.TEMPLATE_ID;
+
+  // Create a new instance of the TemplateRole object for the recipient
+  const signer1 = docusign.TemplateRole.constructFromObject({
+    email: email,
+    name: name,
+    clientUserId: process.env.CLIENT_USER_ID,
+    roleName: 'Applicant',
+  });
+
+  // Add the recipient to the envelope
+  envelope.templateRoles = [signer1];
+
+  // Set the status of the envelope to 'sent'
+  envelope.status = 'sent';
+
+  // Return the envelope object
+  return envelope;
+};
+
+// This function handles the POST request to the /form endpoint
+const handleFormPost = async (request, response) => {
+  // Ensure that the access token is valid and up to date
+  await tokenValidation(request, response);
+
+  // Get an instance of the EnvelopesApi with the configured API client
+  const envelopesApi = getEnvelopesApi(request);
+
+  // Create an envelope object for the provided recipient details
+  const envelope = makeEnvelope(request.body.name, request.body.email);
+
+  // Create the envelope on the DocuSign server
+  const results = await envelopesApi.createEnvelope(process.env.ACCOUNT_ID, {
+    envelopeDefinition: envelope,
+  });
+
+  console.log('Envelope results: ', results);
+  response.send('Envelope created successfully');
+};
+
+app.post('/form', handleFormPost);
 
 app.get('/', async (req, res) => {
   await tokenValidation(req, res);
